5bcd718f3630deb5ea1f1d6bb15c8fc996c3db1c
Dokas <iguinhogms@gmail.com>
2023-06-29 04:30:51 -0300

    v1.01

---
+++ dist/server/flight-server-css-manifest.js
@@ -0,0 +1,6 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {},
+  "cssModules": {
+    "C:\\VSCODE\\IgorDokai-NextJs\\src\\app\\about\\page": []
+  }
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 'use client';
 import Navbar from '@/components/navBar/Navbar';
 
@@ -21,6 +22,27 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#141414" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <title>Igor Dokai</title>
       </head>
       <Body>
